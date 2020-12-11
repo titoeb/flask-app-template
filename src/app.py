a002247058b076961173f33d11e049b7149fec74
titoeb <timtoebrock@gmail.com>
2020-12-11 15:32:22 +0100

    Added experimental post route

--- src/app.py
+++ src/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+@app.route('/add-two-nums', methods=["POST"])
+def add_two_nums():
+	# Extract x, y from the posted data
+	dataDict=request.get_json()
+
+
+	# Add x+y and store it
+	z = dataDict["x"] + dataDict["y"]
+
+	# Prepare json to return the values as 'z'
+	return jsonify({"z": z})
 
 @app.route('/')
 def hello_world():
